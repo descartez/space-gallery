f6224ae24ec04c2bd30cf44538688d40405a102a
Ryan Stong <rvanstong@gmail.com>
2017-07-03 14:20:22 -0700

    adds in window for selected image

--- src/index.js
+++ src/index.js
@@ -10,24 +10,24 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Amanda Nelson Illustration',
-      subtitle: 'Portraiture, Illustration, Design'
+      title: 'Animals in Space',
+      subtitle: 'by Amanda Nelson',
+      tileSize: 0.25,
+      trayHeight: 1.2,
+      selectedImage: "#spaceCorgi"
     };
   }
 
-  changeColor() {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)]
-    });
+  changeImage = () => {
+    console.log("entity entered!")
   }
 
   render () {
     return (
       <Scene>
-        <a-assets>
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+      <a-assets>
+        <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
+        <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
 
           // art textures
           <img id="spaceBunny1" src="https://ucarecdn.com/c6c09887-00a1-4d38-be37-ac1f71551f98/"/>
@@ -37,24 +37,45 @@ class App extends React.Component {
           <img id="classyPheasant" src="https://ucarecdn.com/4743ae78-82f7-4122-8d1f-bb8113c9de2f/"/>
           <img id="dots" src="https://ucarecdn.com/a1895871-999e-41b5-889d-fa534c8aa343/"/>
 
-        </a-assets>
+      </a-assets>
 
-        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
-        <Entity primitive="a-light" type="ambient" color="#445451"/>
-        <Entity primitive="a-light" type="point" intensity="1" position="2 4 4"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity text={{value: this.state.title, align: 'left'}} position={{x: -0.5, y: 2, z: -1}}/>
-        <Entity text={{value: this.state.subtitle, align: 'left'}} position={{x: -0.5, y: 1.9, z: -1}}/>
+          <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+          <Entity primitive="a-light" type="ambient" color="#445451"/>
+          <Entity primitive="a-light" type="point" intensity=".7" position="2 4 5"/>
+          <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+          <Entity text={{value: this.state.title, align: 'left'}} position={{x: -0.5, y: 2, z: -1}} />
+          <Entity text={{value: this.state.subtitle, align: 'left'}} position={{x: -0.5, y: 1.9, z: -1}} />
 
-        <Entity geometry="primitive: plane" material="src: #spaceCorgi" position={{x: 0.5, y: 1.9, z: -1}} />
+          <Entity
+            geometry={{primitive: "plane", width: this.state.tileSize + 1.2, height: this.state.tileSize + 1.2}}
+            material="src: #spaceCorgi"
+            position={{x: 0.5, y: 2, z: -2}} />
+
+          <Entity
+            events={{mouseenter: this.changeImage}}
+            geometry={{primitive: "plane", width: this.state.tileSize, height: this.state.tileSize}}
+            material="src: #spaceBunny1"
+            position={{x: -0.5, y: this.state.trayHeight, z: -1}} />
+
+          <Entity
+            events={{mouseenter: this.changeImage}}
+            geometry={{primitive: "plane", width: this.state.tileSize, height: this.state.tileSize}}
+            material="src: #spaceBunny2"
+            position={{x: 0, y: this.state.trayHeight, z: -1}} />
+
+          <Entity
+            events={{mouseenter: this.changeImage}}
+            geometry={{primitive: "plane", width: this.state.tileSize, height: this.state.tileSize}}
+            material="src: #spaceCorgi"
+            position={{x: 0.5, y: this.state.trayHeight, z: -1}} />
 
 
-        <Entity primitive="a-camera">
+          <Entity primitive="a-camera">
           <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
-        </Entity>
-      </Scene>
-    );
-  }
+          </Entity>
+          </Scene>
+          );
+}
 }
 
 ReactDOM.render(<App/>, document.querySelector('#sceneContainer'));
